06e528596ed95e2d6a01d8d75e9920b0822c9d52
Felipe Lacerda <designerlipe@gmail.com>
2023-07-06 01:10:59 -0300

    Obtendo um post por id

---
+++ src/main/java/com/novagroup/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.novagroup.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.novagroup.workshopmongo.domain.Post;
+import com.novagroup.workshopmongo.repository.PostRepository;
+import com.novagroup.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
